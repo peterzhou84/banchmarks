a6039ef8de88191ec9859acaac6d86d82b8b4420
peterzhou84 <zjzhoufy@126.com>
2017-09-15 13:56:02 +0800

    完成nodejs的服务器开发

---
+++ nodejs/app.js
@@ -0,0 +1,46 @@
+var express = require('express')
+var app = express()
+var fs = require('fs')
+var mysql      = require('mysql');
+
+var pool = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "&Gug124eH",
+    database: "mysql",
+    connectionLimit: 2
+});
+
+var redis = require("redis"),
+client = redis.createClient();
+
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
+ 
+app.get('/1', function (req, res) {
+  res.send('Hello World')
+})
+
+app.get('/2', function (req, res) {
+    fs.readFile('testfile.txt', (err, data) => {
+        if (err) {res.status(501).end();return;}
+            res.send(data + "")
+        });
+})
+  
+app.get('/3', function (req, res) {
+    pool.query("select count(*) from mysql.user",function(err, rst){
+        if (err) {res.status(501).end();return;}
+        res.send('Hello World')
+    });
+})
+
+app.get('/4', function (req, res) {
+    client.get('key1',function(err, rst){
+        if (err) {res.status(501).end();return;}
+        res.send('Hello World')
+    });
+})
+ 
+app.listen(3000)
